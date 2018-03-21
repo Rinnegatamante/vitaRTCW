29eb303c3d3c25ecb8e0466bc9b70afb286dcc3e
Rinnegatamante <rinnegatamante@gmail.com>
2018-03-21 12:41:51 +0100

    Logging to correct folder.

--- code/psp2/con_psp2.c
+++ code/psp2/con_psp2.c
@@ -56,7 +56,7 @@ CON_Print
 */
 void CON_Print(const char *msg) {
 #ifndef RELEASE
-    FILE* f = fopen("ux0:data/ioq3/log.txt", "a+");
+    FILE* f = fopen("ux0:data/iortcw/log.txt", "a+");
 	fwrite(msg, 1, strlen(msg), f);
 	fclose(f);
 #endif
